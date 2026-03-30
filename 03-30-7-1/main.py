46dc837f4ea62d25aa634360f8865ec256c2c2d6
Alexander Fuchsberger <8102991+fuchsberger@users.noreply.github.com>
2026-03-30 11:54:52 -0400

    added speech

--- 03-30-7-1/main.py
+++ 03-30-7-1/main.py
@@ -1,13 +1,47 @@
 import requests
 
-lincoln = "https://gutenberg.org/cache/epub/9/pg9.txt"
-response = requests.get(lincoln)
+import matplotlib.pyplot as pyplot
 
-txt = response.text
 
-indexStart = txt.index("Fellow")
-indexEnd = txt.index("*** END")
+def main():
+    lincoln = "https://gutenberg.org/cache/epub/9/pg9.txt"
+    response = requests.get(lincoln)
 
-speech = txt[indexStart:indexEnd]
+    txt = response.text
 
-print(speech)
+    indexStart = txt.index("Fellow")
+    indexEnd = txt.index("*** END")
+
+    speech = txt[indexStart:indexEnd]
+
+    sentenceList = speech.split(".")
+
+
+    avg_words_per_sentence = mean(sentenceList)
+    print(avg_words_per_sentence)
+
+    senLen = []
+    for s in sentenceList:
+        wordList = s.split()
+        senLen.append(len(wordList))
+
+    print(senLen)
+
+    pyplot.plot(range(1, len(senLen) + 1), senLen, label="Original")
+    pyplot.legend()
+    pyplot.xlabel("Sentence Number")
+    pyplot.ylabel("Word Count")
+    pyplot.show()
+
+def mean(data: list[str]) -> float:
+    if len(data) == 0:
+        raise ValueError("Empty List")
+
+    total = 0
+    for item in data:
+        wordList = item.split()
+        total += len(wordList)
+
+    return total / len(data)
+
+main()
