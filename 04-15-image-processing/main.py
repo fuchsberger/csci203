60e4e9a0270085338942553d7a736eba6682a968
Alexander Fuchsberger <8102991+fuchsberger@users.noreply.github.com>
2026-04-15 10:56:07 -0400

    updated class exercises

--- 04-15-image-processing/main.py
+++ 04-15-image-processing/main.py
@@ -1,17 +1,82 @@
 from PIL import Image
-im = Image.open('./myImage.jpg').convert("RGB")
-pixelMap = im.load()
+import random
 
-img = Image.new( im.mode, im.size)
-pixelsNew = img.load()
-for i in range(img.size[0]):
-    for j in range(img.size[1]):
-        r, g, b = pixelMap[i, j]
+def main():
+    oldPixels, newImage, newPixels = load_image('./myImage.jpg')
 
-        pixelsNew[i,j] = (255, g, b)
+    # Apply correct filter
+    # Only comment in one of those at a time!
+    boost_red(oldPixels, newImage, newPixels)
+    # invert(oldPixels, newImage, newPixels)
+    # grayscale(oldPixels, newImage, newPixels)
+    # set_contrast(oldPixels, newImage, newPixels, 255)
+    # grainy(oldPixels, newImage, newPixels)
+
+    # invertSpot(oldPixels, newImage, newPixels, x1, y1, x2, y2)
+
+    # mirrorHalf(oldPixels, newImage, newPixels)
+
+    # combined(oldPixels, newImage, newPixels)
+
+    # blur(oldPixels, newImage, newPixels, window)
 
 
-im.close()
-img.show()
-img.save("myImage-red.jpg")
-img.close()
+def boost_red(oldPixels, newImage, newPixels):
+    for i in range(newImage.size[0]):
+        for j in range(newImage.size[1]):
+            r, g, b = oldPixels[i, j]
+
+            newPixels[i, j] = (255, g, b)
+
+    save_image(newImage, "myImage-red.jpg")
+
+def invert(oldPixels, newImage, newPixels):
+    for i in range(newImage.size[0]):
+        for j in range(newImage.size[1]):
+            # TODO
+            pass
+
+    save_image(newImage, "myImage-invert.jpg")
+
+def grayscale(oldPixels, newImage, newPixels):
+    for i in range(newImage.size[0]):
+        for j in range(newImage.size[1]):
+            # TODO
+            pass
+
+    save_image(newImage, "myImage-grayscale.jpg")
+
+def set_contrast(oldPixels, newImage, newPixels, C):
+    for i in range(newImage.size[0]):
+        for j in range(newImage.size[1]):
+            # TODO
+            pass
+
+    save_image(newImage, "myImage-set-contrast.jpg")
+
+def grainy(oldPixels, newImage, newPixels):
+    for i in range(newImage.size[0]):
+        for j in range(newImage.size[1]):
+            # TODO
+            pass
+    save_image(newImage, "myImage-set-contrast.jpg")
+
+
+# Helpers below, do not touch
+
+def load_image(filename):
+    oldImage = Image.open(filename).convert("RGB")
+    oldPixels = oldImage.load()
+    oldImage.close()
+
+    newImage = Image.new(oldImage.mode, oldImage.size)
+    newPixels = newImage.load()
+
+    return oldPixels, newImage, newPixels
+
+def save_image(image, name):
+    image.show()
+    image.save(name)
+    image.close()
+
+main()
